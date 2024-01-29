0cd47c95e5198192595ec17266c612c282ba1e9a
musfatihii <musfatihii@gmail.com>
2024-01-29 11:44:14 +0100

    project init

---
+++ src/main/java/ma/musfatihii/reviewrave/ReviewraveApplication.java
@@ -0,0 +1,13 @@
+package ma.musfatihii.reviewrave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewraveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewraveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/musfatihii/reviewrave/ReviewraveApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.musfatihii.reviewrave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewraveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
